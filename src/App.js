7b7af436db7fe350f6a8c0a99c821a1731b2e5cc
ayoub benouahi <abenouahi5@gmail.com>
2022-09-30 11:37:58 +0100

    Add login Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import  NavBar from './components/header/header';
+import  Login from './components/login/login';
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <Login />
       {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>  */}
-      <p>fezzf</p>
+ 
       </div>
       
   );
---
+++ src/components/login/login.js
@@ -0,0 +1,17 @@
+import './Login.css';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div className='Login'>
+        
+      <p className=''>Login</p>
+      
+      </div>
+      
+  );
+}
+
+
+export default Login;
