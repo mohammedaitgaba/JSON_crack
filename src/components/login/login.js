f1acb041eb81ae6d8df17644adb1b9231cacd38b
ayoub benouahi <abenouahi5@gmail.com>
2022-10-03 11:23:52 +0100

    Fini Compenent Login et Register

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,56 +2,128 @@ import './Login.css';
 
 import React, {useState}  from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import Register from '../Register/Register';
 // import Register from '../Register/Register';
 
 
 const Login = ({open,onClose}) =>{
-
+    const [register,setRegister] = useState(false)
     // const [openRegister, setOpenRegister ] = useState(false);
-
-
-    if(!open ) return null
-    return(
-        <div className='Login'> 
+    
+    const gotoRegister = ()=>{
         
-        <div className='NumLogo'>
-            <p class="logo">
-            JSON
-             <span>crack</span> 
-            </p>
-        </div>  
-        <div> 
-            <p className='Lougin' >Lougin</p>
-        </div>
+        setRegister(true)
+        console.log(register);
+    }
+    const gotoLogin = ()=>{
+        setRegister(false)
+    }
+    if(!open ) return null
 
-        <div>
-            <div>
-                <p className='user'>UserNAme</p>
-                <input className='InputUser' type='text'></input>
+    if (register) {
+        return (
+            <div className='LoginContainer'>
+            <div className='Rougister'> 
+            <div className='Close'>  <i class="fal fa-times-circle"  onClick={onClose}></i></div>
+          
+            
+            
+            <div className='NumLogo'>
+                <p class="logo">
+                JSON
+                 <span>crack</span> 
+                </p>
+            </div>  
+            <div> 
+                <p className='Lougin' >Register</p>
+                
+                
             </div>
+    
             <div>
-                <p className='user'>Password</p>
-                <input className='InputUser' type='Password'></input>
-                <p className='CreeCompte'>Cree un Compte??</p>
+                <div>
+                    <p className='user'>UserNAme</p>
+                    <input className='InputUser' type='text'></input>
+                </div>
+              
+                <div>
+                    <p className='user'>Password</p>
+                    <input className='InputUser' type='Password'></input>
+                    <p className='CreeCompte' onClick={gotoLogin}>login</p>
+                </div>
+                
+                <button className='ButtonLougin'>Register</button>
+               
+                
+    
+                
             </div>
-            <button className='ButtonLougin' onClick={onClose} >Close</button>
-            <button className='ButtonLougin'>Lougin</button>
+            
+            
+            
            
             
-
+    
             
-        </div>
-        
-        
-        
-       
-        
+             </div>
+             </div>
+             )
+    
+    } else {
+        return (
+            <div className='LoginContainer'>
+            <div className='Login'> 
+            <div className='Close'>  <i class="fal fa-times-circle"  onClick={onClose}></i></div>
+          
+            
+            
+            <div className='NumLogo'>
+                <p class="logo">
+                JSON
+                 <span>crack</span> 
+                </p>
+            </div>  
+            <div> 
+                <p className='Lougin' >Login</p>
+                
+                
+            </div>
+    
+            <div>
+                <div>
+                    <p className='user'>UserNAme</p>
+                    <input className='InputUser' type='text'></input>
+                </div>
+                <div>
+                    <p className='user'>Password</p>
+                    <input className='InputUser' type='Password'></input>
+                    <p className='CreeCompte' onClick={gotoRegister}>Cree un Compte??</p>
+                </div>
+                
+                <button className='ButtonLougin'>Lougin</button>
+               
+                
+    
+                
+            </div>
+            
+            
+            
+           
+            
+    
+            
+             </div>
+             </div>
+        )
+    
+    } 
+    
+    
 
         
-         </div>
-        
-    )
-}
+        }
+
 
 
 export default Login;
